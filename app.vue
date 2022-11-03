0fc930c8693e0d09d9d7c6e6d3d6b97c9b83fffc
MrGultekin <gultekin.de@gmail.com>
2022-11-03 09:18:31 +0100

    converted into compositional API

--- app.vue
+++ app.vue
@@ -1,26 +1,29 @@
-<script>
-import {defineNuxtComponent} from "nuxt/app";
+<script setup >
+// import {defineNuxtComponent} from "nuxt/app";
+import {computed,ref} from "vue";
 
-export default defineNuxtComponent({
-  data: () => ({
-    todoList: []
-  }),
-  methods: {
-    fetchTodoList() {
+// export default defineNuxtComponent({
+//   data: () => ({
+  let todoList = ref([])
+  // }),
+  // methods: {
+  function fetchTodoList() {
       fetch('https://jsonplaceholder.typicode.com/todos/')
           .then(response => response.json())
-          .then(json => this.todoList = json)
+          .then(json => todoList.value = json)
     }
-  },
-  computed: {
-    completedTodos(){
-      return this.todoList.filter(todo => todo.completed);
-    },
-    remainingTodos(){
-      return this.todoList.filter(todo => !todo.completed);
-    }
-  }
-})
+
+  // computed: {
+   const completedTodos = computed(()=>{
+     return todoList.value.filter(todo => todo.completed)
+   })
+
+    // remainingTodos() {
+    //   return this.todoList.filter(todo => !todo.completed);
+    // }
+  const remainingTodos = computed(()=>{
+    return todoList.value.filter(todo => !todo.completed);
+  })
 
 </script>
 
@@ -37,9 +40,9 @@ export default defineNuxtComponent({
     </p>
     <h1>Mustafa`s Nuxt</h1>
     <button @click="fetchTodoList"> Click to fetch Data</button>
-    <p>Total todos :{{todoList.length}}</p>
-    <p>Completed todos :{{completedTodos.length}}</p>
-    <p>Uncompleted todos :{{remainingTodos.length}}</p>
+    <p>Total todos :{{ todoList.length }}</p>
+    <p>Completed todos :{{ completedTodos.length }}</p>
+    <p>Uncompleted todos :{{ remainingTodos.length }}</p>
     <!--    <pre>-->
     <!--      {{todoList}}-->
     <!--    </pre>-->
@@ -52,3 +55,61 @@ export default defineNuxtComponent({
     </ul>
   </div>
 </template>
+
+
+
+
+<!--<script>-->
+<!--import {defineNuxtComponent} from "nuxt/app";-->
+
+<!--export default defineNuxtComponent({-->
+<!--  data: () => ({-->
+<!--    todoList: []-->
+<!--  }),-->
+<!--  methods: {-->
+<!--    fetchTodoList() {-->
+<!--      fetch('https://jsonplaceholder.typicode.com/todos/')-->
+<!--          .then(response => response.json())-->
+<!--          .then(json => this.todoList = json)-->
+<!--    }-->
+<!--  },-->
+<!--  computed: {-->
+<!--    completedTodos() {-->
+<!--      return this.todoList.filter(todo => todo.completed);-->
+<!--    },-->
+<!--    remainingTodos() {-->
+<!--      return this.todoList.filter(todo => !todo.completed);-->
+<!--    }-->
+<!--  }-->
+<!--})-->
+
+<!--</script>-->
+
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <img src="/todo.jpg" alt="Eden Constantino">-->
+<!--    <p>-->
+<!--      Photo by <a-->
+<!--        href="https://unsplash.com/@edenconstantin0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eden-->
+<!--      Constantino</a> on <a-->
+<!--        href="https://unsplash.com/s/photos/todos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>-->
+
+<!--    </p>-->
+<!--    <h1>Mustafa`s Nuxt</h1>-->
+<!--    <button @click="fetchTodoList"> Click to fetch Data</button>-->
+<!--    <p>Total todos :{{ todoList.length }}</p>-->
+<!--    <p>Completed todos :{{ completedTodos.length }}</p>-->
+<!--    <p>Uncompleted todos :{{ remainingTodos.length }}</p>-->
+<!--    &lt;!&ndash;    <pre>&ndash;&gt;-->
+<!--    &lt;!&ndash;      {{todoList}}&ndash;&gt;-->
+<!--    &lt;!&ndash;    </pre>&ndash;&gt;-->
+
+<!--    <ul>-->
+<!--      <li v-for="todo in todoList" :key=" `todo-id-${todo.id}` ">-->
+<!--        <input type="checkbox" :checked="todo.completed"/>-->
+<!--        {{ todo.title }}-->
+<!--      </li>-->
+<!--    </ul>-->
+<!--  </div>-->
+<!--</template>-->
